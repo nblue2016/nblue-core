ee7882b13c83895d2132067ef72307e872e9c475
Yifeng Jiang <yifeng.jiang@mkcorp.com>
2016-12-02 13:27:58 +0800

    fix one issue in config map

--- test/native/map.js
+++ test/native/map.js
@@ -2,11 +2,16 @@ const assert = require('assert')
 
 describe('map', () => {
   const map = new Map()
+  const mapArray = new Map()
 
   map.
     set('key1', 'val1').
     set('key2', 'val2').
     set('key3', 'val3')
+
+  mapArray.set('ary1', null).
+    set('ary2', 's1').
+    set('ary3', ['s1', 's2', 's3'])
 
   it('method of get2', () => {
     assert.equal(map.get('key1'), 'val1', 'ok')
@@ -34,5 +39,12 @@ describe('map', () => {
     }
 
     assert.deepEqual(map.toJSON(), JSON.stringify(target), 'ok')
+  })
+
+  it('method of getArray', () => {
+    assert.deepEqual(mapArray.getArray('ary1'), [], 'ok')
+    assert.deepEqual(mapArray.getArray('ary2'), ['s1'], 'ok')
+    assert.deepEqual(mapArray.getArray('ary3'), ['s1', 's2', 's3'], 'ok')
+    assert.deepEqual(mapArray.getArray('ary4'), [], 'ok')
   })
 })
