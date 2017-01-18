a4fe1210e1e67b1580d4a51280fd8662a6651b82
Bruce Jiang <nblue2016@163.com>
2017-01-18 09:12:30 +0800

    add reference for uuid

--- test/start.js
+++ test/start.js
@@ -1,5 +1,18 @@
-require('../lib/')
+const assert = require('assert')
+const nblue = require('../lib/')
 
+// declare types in library
+const StringBuilder = nblue.StringBuilder
+const UUID = nblue.UUID
+
+const sb = new StringBuilder()
+
+sb.append('test')
+
+assert.ok(sb.toString(), 'string builder')
+assert.ok(UUID.generate(), 'uuid')
+
+// declare array of test scripts
 const scripts = [
   './conf/conf.js',
   './logger/log.js',
